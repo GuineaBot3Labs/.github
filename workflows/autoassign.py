fce30472d8082a977143da027f540a7dc53f0c67
nope! no name for you to hear! none at all! <107788660+ethicalhacker7192@users.noreply.github.com>
2023-12-15 11:55:07 -0700

    Update autoassign.py

--- workflows/autoassign.py
+++ workflows/autoassign.py
@@ -1,25 +1,43 @@
-name: Auto Assign to Organization Member
+import requests
+import random
+import os
 
-on:
-  issues:
-    types: [opened]
-  pull_request:
-    types: [opened]
+def get_org_members(org_name, token):
+    url = f"https://api.github.com/orgs/{org_name}/members"
+    headers = {
+        'Authorization': f'token {token}',
+        'Accept': 'application/vnd.github.v3+json'
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return [member['login'] for member in response.json()]
+    else:
+        raise Exception(f"Failed to fetch organization members: {response.content}")
 
-jobs:
-  assign-random-member:
-    runs-on: ubuntu-latest
-    permissions:
-      issues: write
-      pull-requests: write
-    steps:
-      - name: Checkout repository
-        uses: actions/checkout@v3  # Updated to the latest version
-      
-      - name: Run custom assignment script
-        run: python ../../scripts/assign_member_to_issues.py
-        env:
-          GITHUB_TOKEN: ${{ secrets.MY_PAT }}
-          ORG_NAME: 'GuineaBot3Labs'  # Ensure this is correct
-          REPO_NAME: ${{ github.repository }}  # Verify this resolves correctly
-          ISSUE_NUMBER: ${{ github.event.issue.number || github.event.pull_request.number }}
+def assign_issue(issue_number, assignee, repo_name, token):
+    url = f"https://api.github.com/repos/{repo_name}/issues/{issue_number}/assignees"
+    headers = {
+        'Authorization': f'token {token}',
+        'Accept': 'application/vnd.github.v3+json'
+    }
+    data = {
+        'assignees': [assignee]
+    }
+    response = requests.post(url, json=data, headers=headers)
+    if response.status_code not in [200, 201]:
+        raise Exception(f"Failed to assign issue: {response.content}")
+
+def main():
+    token = os.getenv('GITHUB_TOKEN')
+    org_name = os.getenv('ORG_NAME')
+    repo_name = os.getenv('REPO_NAME')
+    issue_number = os.getenv('ISSUE_NUMBER')
+
+    members = get_org_members(org_name, token)
+    assignee = random.choice(members)
+
+    assign_issue(issue_number, assignee, repo_name, token)
+    print(f"Issue {issue_number} has been assigned to {assignee}")
+
+if __name__ == "__main__":
+    main()
